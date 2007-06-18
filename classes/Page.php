34b03a9bd7c4ef78fc6d30b13a9c19ec67f30357
pbihler <bihler@cs.uni-bonn.de>
2007-06-18 11:35:48 +0000

    Disclaimer

--- classes/Page.php
+++ classes/Page.php
@@ -49,7 +49,7 @@
          	echo "    <div id=\"introduction\">" . $this->introduction . "</div>\n";
          echo "    <div id=\"wrapperContent\">\n";
          $this->renderNotes();
-         $this->renderNote('All data is provided for informational purposes only and no responsibility is taken for the correctness of the information.<br /><br />Die hier angezeigten Daten dienen lediglich Informationszwecken.<br />Alle Informationen ohne Gew&auml;hr.','Important remark - Wichtiger Hinweis');
+         $this->renderNote('<b>Disclaimer:</b> All data is provided for informational purposes only and no responsibility is taken for the correctness of the information.<br /><br /><b>Haftungsausschluss:</b> Die hier angezeigten Daten dienen lediglich Informationszwecken.<br />Alle Informationen ohne Gew&auml;hr.','Important remark - Wichtiger Hinweis');
          echo "    </div>\n";
      }
      
